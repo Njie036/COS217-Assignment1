db991161958ddf715cee9ed9a4aecbf9968ff093
Ndongo Njie <ndongonjie17@gmail.com>
2024-02-10 17:28:59 -0500

    cccc

--- decomment.c
+++ decomment.c
@@ -148,7 +148,7 @@ int main(void)
     int inputChar;
     enum Statetype state = INITIAL;
     int lineCount = 1;
-    int whereCommentStarts = 0;
+    int whereCommentStarts = 1;
     while ((inputChar = getchar()) != EOF) {
         switch (state) {
             case INITIAL:
